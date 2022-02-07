411906536dbaa1b97f9474a0fe947c634d20cac7
Stephen Strange <80538114+Strange-bs@users.noreply.github.com>
2022-02-07 23:14:00 +0500

    Updated script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
+class Person {
+  constructor(name, age){
+    this.name= name;
+    this.age = age;
+  }
+  method1() {
+    console.log(this)
+    console.log(typeof this)
+  }
+  run() {
+    console.log(this.name + ' ran away')
+    console.log("when he was " + this.age)
+  }
+}
 
+let person = new Person('John ', 5)
+// person.method1()
+person.run()
